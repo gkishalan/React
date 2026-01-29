6c147b06f91e61e5ddb23b5b354d455f373b5878
gkishalan <princekishalan655@gmail.com>
2026-01-29 20:57:31 +0530

    Created Course Folder in src and moved the course.jsx into it

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar'
-import Course from './Course'
+import Course from './Course/Course'
 import Footer from './footer'
 
 function App() {
