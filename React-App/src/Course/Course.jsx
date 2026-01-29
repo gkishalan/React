946cdb3440ab1456620c2a58b3e5dbe741c101be
gkishalan <princekishalan655@gmail.com>
2026-01-29 21:08:25 +0530

    Imported Course.module.css in Course.jsx and changed the background color

--- React-App/src/Course/Course.jsx
+++ React-App/src/Course/Course.jsx
@@ -1,6 +1,8 @@
+import styles from './Course.module.css'
+
 function Course(){
     return(
-        <div className = "card">
+        <div className = {styles.card}>
             <img src="" alt="" />
             <h3>HTML</h3>
             <p>This is Begginer level course to learn all about HTML </p>
