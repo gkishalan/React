8b6282a0e6dbfcbec4530fa91472ba79c10118a9
gkishalan <princekishalan655@gmail.com>
2026-01-29 20:52:17 +0530

    Created className card in Course.jsx

--- React-App/src/Course.jsx
+++ React-App/src/Course.jsx
@@ -1,6 +1,6 @@
 function Course(){
     return(
-        <div>
+        <div className = "card">
             <img src="" alt="" />
             <h3>HTML</h3>
             <p>This is Begginer level course to learn all about HTML </p>
