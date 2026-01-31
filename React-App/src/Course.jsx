b9480d6e12adbf7add487be802a1daa6b05d274c
gkishalan <princekishalan655@gmail.com>
2026-02-01 04:55:38 +0530

    Handling Missing Data with props.name

--- React-App/src/Course.jsx
+++ React-App/src/Course.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 function Course(props){
 
         return(
-        <div className = "card">
+        props.name && <div className = "card">
             <img src = {props.image} alt="" />
             <h3>{props.name}</h3>
             <p> {props.price} </p>
             <span> {props.rate}</span>
         </div>
-    );
+        );
 
 Course.propTypes = {
     name : PropTypes.string,
